6a0a58776928e1f2f7f2393c06838e4a89ef2a38
Jacob Feng <peanutchowder@gmail.com>
2024-10-18 15:46:01 -0600

    allow mobile testing

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true
+    open: true,
+    host: '0.0.0.0',
+    port: 3000
   },
   build: {
     outDir: 'dist',
